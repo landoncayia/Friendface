78098c76bb9da215e2086a90bd96e3865c21a376
Landon Cayia <landon.cayia@gmail.com>
2022-08-12 09:55:24 -0400

    Create Friend struct, add Identifiable to User
    Now, both the Friend and Users structs comform to both Codable and
    Identifiable due to their string id.

---
+++ Friendface/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  Friendface
+//
+//  Created by Landon Cayia on 8/12/22.
+//
+
+import Foundation
+
+struct Friend: Codable, Identifiable {
+    var id: String
+    var name: String
+}
--- Friendface/User.swift
+++ Friendface/User.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct User: Codable {
+struct User: Codable, Identifiable {
     var id: String
     var isActive: Bool
     var name: String
