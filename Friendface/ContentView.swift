f76adb296c0263caaa07016fa6880f00672bb094
Landon Cayia <landon.cayia@gmail.com>
2022-08-12 10:11:52 -0400

    Add basic list of items to ContentView
    For now, a button that adds example user(s) has been added to the
    toolbar in ContentView; this will be removed when it is no longer
    necessary.

--- Friendface/ContentView.swift
+++ Friendface/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var userList = Users(users: [User]())
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(userList.users) { user in
+                VStack(alignment: .leading) {
+                    Text(user.name)
+                        .font(.headline)
+                    Text(user.email)
+                }
+            }
+            .navigationTitle("Friendface")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Add Examples") {
+                        let james = User(id: "123456", isActive: false, name: "James Jenkins", age: 34, company: "Apple", email: "jjenkins@apple.com", address: "42 California Rd, Cupertino, CA 94024", about: "I have been a dedicated iOS developer since the beginning.", registered: Date(), tags: ["happy", "booknerd"])
+                        userList.users.append(james)
+                    }
+                }
+            }
+        }
     }
 }
 
