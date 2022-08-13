18e2a03643273782a0b4af875c01f510e9546ee4
Landon Cayia <landon.cayia@gmail.com>
2022-08-13 13:21:15 -0400

    Create Core Data controller and add to environment
    The merge policy in the data controller was set to
    "mergeByPropertyObjectTrump"

---
+++ Friendface/DataController.swift
@@ -0,0 +1,24 @@
+//
+//  DataController.swift
+//  Friendface
+//
+//  Created by Landon Cayia on 8/13/22.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Friendface")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+                return
+            }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        }
+    }
+}
